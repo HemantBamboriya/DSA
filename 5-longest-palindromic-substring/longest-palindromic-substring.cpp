0722ae66c3eede153765ce7d5d1af5a6b4c05576
Hemant Bamboriya <dhakadhemant370@gmail.com>
2026-07-05 22:29:48 +0530

    Time: 177 ms (21.52%) | Memory: 13.8 MB (45.20%) - LeetSync

---
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+int t[1001][1001];
+ bool solve(string &s ,int i,int j){
+    if(i>=j) return true;
+
+    if(t[i][j]!= -1){
+        return t[i][j];
+    }
+
+    if(s[i]==s[j]){
+      return t[i][j]= solve(s,i+1,j-1);
+    }
+
+    return t[i][j]=false;
+
+ }
+
+    string longestPalindrome(string s) {
+        int n=s.length();
+        int startIndex=0;
+        int maxlen= INT_MIN;
+        memset(t,-1,sizeof(t));
+        
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                if(solve(s,i,j)){
+                    if(j-i+1>maxlen){
+                    maxlen= j-i+1;
+                    startIndex=i;
+                }
+                }
+            }
+        }
+        return s.substr(startIndex,maxlen);
+    }
+};
