ba5946b9093fb06db160b39b455ecc3726e912ec
Hemant Bamboriya <dhakadhemant370@gmail.com>
2026-07-06 23:48:30 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (81.86%) - LeetSync

--- 231-power-of-two/power-of-two.cpp
+++ 231-power-of-two/power-of-two.cpp
@@ -2,10 +2,7 @@ class Solution {
 public:
     bool isPowerOfTwo(int n) {
         if(n<=0) return false;
-        while(n%2==0){
-            n= n/2;
-        }
-        return n==1;
+       return ((n&(n-1))==0);
         
     }
 };
