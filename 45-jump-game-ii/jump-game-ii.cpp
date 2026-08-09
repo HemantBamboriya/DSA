5330069a65b73a192919c7abe3875c7cf8ba2cfa
Hemant Bamboriya <dhakadhemant370@gmail.com>
2026-08-09 23:59:59 +0530

    Time: 3 ms (28.25%) | Memory: 20.7 MB (29.36%) - LeetSync

---
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        if(nums.size()==1) return 0;
+        int destination=nums.size()-1;
+        int totalJumps=0;
+        int lastJumpIndex=0;
+        int coverage=0;
+        for(int i=0;i<nums.size();i++){
+            coverage=max(coverage,i+nums[i]);
+            if(i==lastJumpIndex){
+            lastJumpIndex=coverage;
+            totalJumps++;
+            if(coverage>=destination){
+                return totalJumps;
+            }
+            }
+            
+        }
+        return totalJumps;
+        
+    }
+};
