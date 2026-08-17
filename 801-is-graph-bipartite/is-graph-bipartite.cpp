09056dac87dff2f2ac11b8613fd00955125656cd
Hemant Bamboriya <dhakadhemant370@gmail.com>
2026-08-17 23:56:00 +0530

    Time: 0 ms (100.00%) | Memory: 17.4 MB (79.16%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.cpp
+++ 801-is-graph-bipartite/is-graph-bipartite.cpp
@@ -20,12 +20,24 @@ bool check(int start,vector<int>&color,vector<vector<int>>&graph){
     return true;
 }
 
+bool dfs(int node,int col,vector<int>&color,vector<vector<int>>&graph){
+   color[node]=col;
+   for(auto it:graph[node]){
+    if(color[it]==-1){
+        if(dfs(it,!col,color,graph)==false) return false;
+    }else if(color[it]==col){
+        return false;
+    }
+   }
+   return true;
+}
+
     bool isBipartite(vector<vector<int>>& graph) {
         int n=graph.size();
         vector<int>color(n,-1);
         for(int i=0;i<n;i++){
             if(color[i]==-1){
-            if(check(i,color,graph)==false){
+            if(dfs(i,0,color,graph)==false){
                 return false;
             }
         }
