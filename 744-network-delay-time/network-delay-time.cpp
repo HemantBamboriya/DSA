402e52e5a7221f8afa98b683b40c3ee89293fc8a
Hemant Bamboriya <dhakadhemant370@gmail.com>
2026-08-20 00:18:20 +0530

    Time: 98 ms (15.97%) | Memory: 47.7 MB (34.04%) - LeetSync

--- 744-network-delay-time/network-delay-time.cpp
+++ 744-network-delay-time/network-delay-time.cpp
@@ -1,44 +1,41 @@
 class Solution {
 public:
-
-vector<int>dijkstra(int vertices,int source,vector<vector<pair<int,int>>>adj){
-    vector<int>dist(vertices+1,INT_MAX);
-    dist[source]=0;
-    priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
-    pq.push({0,source});
+vector<int>dijsktra(int n,int src,vector<pair<int,int>>adj[]){
+    priority_queue< pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
+    pq.push({0,src});//dist,node;
+    vector<int>dist(n+1,1e9);
+    dist[src]=0;
     while(!pq.empty()){
-        int d=pq.top().first;
-        int u=pq.top().second;
+        auto it=pq.top();
         pq.pop();
-        if(d>dist[u]) continue;
-        for(auto edge:adj[u]){
-            int v=edge.first;
-            int weight=edge.second;
-            if(dist[u] !=INT_MAX && weight+dist[u]<dist[v]){
-                dist[v]=weight+dist[u];
-                pq.push({dist[v],v});
+        int node=it.second;
+        int dis=it.first;
+        if(dis>dist[node]) continue;
+        for(auto it:adj[node]){
+            int adjNode=it.first;
+            int adjWt=it.second;
+            if(adjWt+dis<dist[adjNode]){
+                dist[adjNode]=adjWt+dis;
+                pq.push({dist[adjNode],adjNode});
             }
         }
-    }return dist;
+    }
+    return dist;
 }
 
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        vector<vector<pair<int,int>>>adj(n+1);
-        for(auto & edge:times){
-            int u=edge[0];
-            int v=edge[1];
-            int w=edge[2];
-            adj[u].push_back({v,w});
+        vector<pair<int,int>>adj[n+1];
+        for(auto it:times){
+            adj[it[0]].push_back({it[1],it[2]});
         }
-        vector<int>distance=dijkstra(n,k,adj);
-        int maxi=0;
-        for(int i=1;i<=n;i++){
-            if(distance[i]==INT_MAX){
+        int res=0;
+        vector<int>ans=dijsktra(n,k,adj);
+        for(int i=1;i<ans.size();i++){
+            if(ans[i]==1e9){
                 return -1;
             }
-            maxi=max(maxi,distance[i]);
-
+            res=max(ans[i],res);
         }
-        return maxi;
+        return res;
     }
 };
