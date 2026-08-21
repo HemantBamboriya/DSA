ec61282b722762bbe68d9f831827ebea53e582f3
Hemant Bamboriya <dhakadhemant370@gmail.com>
2026-08-21 12:15:03 +0530

    Time: 47 ms (35.62%) | Memory: 49.5 MB (27.38%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,29 +1,27 @@
 class Solution {
 public:
-int minCoinChange(int index,int target,vector<int>&coins,vector<vector<int>>&dp){
-    if(index==0){
-        if(target%coins[index]==0){
+int solve(int idx,vector<int>&coins,int target,vector<vector<int>>&dp){
+    if(idx==0){
+        if(target%coins[0]==0){
             return target/coins[0];
-        }else{
-            return 1e9;
         }
+        return 1e9;
     }
-    if(dp[index][target]!=-1) return dp[index][target];
-    int nottake=minCoinChange(index-1,target,coins,dp);
-    int take=INT_MAX;
-    if(coins[index]<=target){
-        take= 1+minCoinChange(index,target-coins[index],coins,dp);
+    if(dp[idx][target]!=-1) return dp[idx][target];
+    int nottake=solve(idx-1,coins,target,dp);
+    int take=1e9;
+    if(coins[idx]<=target){
+        take=1+solve(idx,coins,target-coins[idx],dp);
     }
-    return dp[index][target]=min(take,nottake);
+    return dp[idx][target]=min(take,nottake);
 }
+
 
     int coinChange(vector<int>& coins, int amount) {
         int n=coins.size();
         vector<vector<int>>dp(n,vector<int>(amount+1,-1));
-        int ans=minCoinChange(n-1,amount,coins,dp);
-        if(ans==1e9){
-            return -1;
-        }
+        int ans=solve(n-1,coins,amount,dp);
+        if(ans>=1e9) return -1;
         return ans;
         
     }
