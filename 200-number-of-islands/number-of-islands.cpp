0132b7922eaf67e6e54b68d735203b98093e4d96
Hemant Bamboriya <dhakadhemant370@gmail.com>
2026-08-22 12:37:34 +0530

    Time: 30 ms (33.63%) | Memory: 24.1 MB (21.93%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,28 +1,27 @@
 class Solution {
 public:
-void bfs(int row,int col,vector<vector<char>>&grid,vector<vector<int>>&visited){
-    int n=grid.size();
-    int m=grid[0].size();
-    queue<pair<int,int>>pq;
-    pq.push({row,col});
-    visited[row][col]=1;
-    int delrow[4]={-1,0,1,0};
-    int delcol[4]={0,1,0,-1};
-    while(!pq.empty()){
-        int row=pq.front().first;
-        int col=pq.front().second;
-        pq.pop();
-        for(int i=0;i<4;i++){
-            int nrow=row+delrow[i];
-            int ncol=col+delcol[i];
-            if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && !visited[nrow][ncol] && grid[nrow][ncol]=='1'){
-                visited[nrow][ncol]=1;
-                pq.push({nrow,ncol});
-            }
+
+void bfs(int row,int col,vector<vector<char>>&grid,vector<vector<int>>&visited,int n,int m){
+   queue<pair<int,int>>q;
+   q.push({row,col});
+   visited[row][col]=1;
+   int delrow[]={-1,0,1,0};
+   int delcol[]={0,1,0,-1};
+   while(!q.empty()){
+    int r=q.front().first;
+    int c=q.front().second;
+    q.pop();
+    for(int i=0;i<4;i++){
+        int nrow=r+delrow[i];
+        int ncol=c+delcol[i];
+        if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && visited[nrow][ncol]==0 && grid[nrow][ncol]=='1'){
+            q.push({nrow,ncol});
+            visited[nrow][ncol]=1;
         }
     }
-}
 
+   }
+}
 
     int numIslands(vector<vector<char>>& grid) {
         int n=grid.size();
@@ -33,8 +32,9 @@ void bfs(int row,int col,vector<vector<char>>&grid,vector<vector<int>>&visited){
             for(int j=0;j<m;j++){
                 if(!visited[i][j] && grid[i][j]=='1'){
                     count++;
-                    bfs(i,j,grid,visited);
+                    bfs(i,j,grid,visited,n,m);
                 }
+
             }
         }
         return count;
