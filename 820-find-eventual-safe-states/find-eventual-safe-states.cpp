2b9ac8f4d13631177db5d5469e61d7b529c15f90
Hemant Bamboriya <dhakadhemant370@gmail.com>
2026-08-22 13:48:53 +0530

    Time: 49 ms (45.34%) | Memory: 63.6 MB (55.20%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.cpp
+++ 820-find-eventual-safe-states/find-eventual-safe-states.cpp
@@ -2,10 +2,10 @@ class Solution {
 public:
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
         int n=graph.size();
-        vector<int>adj[n];
         vector<int>indegree(n,0);
+        vector<int>adj[n];
         for(int i=0;i<n;i++){
-            for(auto & it:graph[i]){
+            for(auto it:graph[i]){
                 adj[it].push_back(i);
                 indegree[i]++;
             }
@@ -18,18 +18,19 @@ public:
         }
         vector<int>topo;
         while(!q.empty()){
-          int node=q.front();
-          q.pop();
-          topo.push_back(node);
-          for(auto it:adj[node]){
-            indegree[it]--;
-            if(indegree[it]==0){
-                q.push(it);
+            int node=q.front();
+            q.pop();
+            topo.push_back(node);
+            for(auto it:adj[node]){
+                indegree[it]--;
+                if(indegree[it]==0){
+                    q.push(it);
+                }
             }
-          }
+
         }
-sort(topo.begin(),topo.end());
-return topo;
+        sort(topo.begin(),topo.end());
+        return topo;
         
     }
 };
