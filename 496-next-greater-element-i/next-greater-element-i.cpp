767f36ad949d53848cedaf48432cdfc5b8bffb6a
Hemant Bamboriya <dhakadhemant370@gmail.com>
2026-08-27 18:56:39 +0530

    Time: 9 ms (6.43%) | Memory: 12.3 MB (85.80%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        int n1=nums1.size();
-        int n2=nums2.size();
-        vector<int>nge(n2);
+        int n=nums2.size();
+        vector<int>nge(n);
         stack<int>st;
-        for(int i=n2-1;i>=0;i--){
-            while(!st.empty() && nums2[i]>=st.top()){
+        for(int i=n-1;i>=0;i--){
+            while(!st.empty() && st.top()<=nums2[i]){
                 st.pop();
             }
             if(st.empty()){
@@ -16,14 +15,16 @@ public:
             }
             st.push(nums2[i]);
         }
-        vector<int>ans(n1);
-        for(int i=0;i<n1;i++){
-            for(int j=0;j<n2;j++){
-                if(nums1[i]==nums2[j]){
+        vector<int>ans(nums1.size());
+        for(int i=0;i<nums1.size();i++){
+            int ele=nums1[i];
+            for(int j=0;j<n;j++){
+                if(ele==nums2[j]){
                     ans[i]=nge[j];
                 }
             }
         }
-       return ans; 
+return ans;
+        
     }
 };
