93b6187e7ff2c144597ae07d072753a83719baf1
Hemant Bamboriya <dhakadhemant370@gmail.com>
2026-09-11 00:10:05 +0530

    Time: 0 ms (100.00%) | Memory: 12 MB (53.80%) - LeetSync

--- 2005-check-if-all-the-integers-in-a-range-are-covered/check-if-all-the-integers-in-a-range-are-covered.cpp
+++ 2005-check-if-all-the-integers-in-a-range-are-covered/check-if-all-the-integers-in-a-range-are-covered.cpp
@@ -1,17 +1,21 @@
 class Solution {
 public:
     bool isCovered(vector<vector<int>>& ranges, int left, int right) {
-        set<int>st;
-        for(int j=0;j<ranges.size();j++){
-           for(int i=ranges[j][0];i<=ranges[j][1];i++){
-            st.insert(i);
-           }
+        vector<int>diff(52,0);
+        for(int i=0;i<ranges.size();i++){
+            int start=ranges[i][0];
+            int end=ranges[i][1];
+            diff[start]++;
+            diff[end+1]--;
         }
-      for(int i=left;i<=right;i++){
-        if(!st.count(i)){
-            return false;
+        for(int i=1;i<52;i++){
+            diff[i] +=diff[i-1];
         }
-      }
-      return true;
+        for(int i=left;i<=right;i++){
+            if(diff[i]==0){
+                return false;
+            }
+        }
+        return true;
     }
 };
