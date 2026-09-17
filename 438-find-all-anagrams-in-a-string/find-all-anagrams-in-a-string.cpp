250243bdd19f5da806459ea0fb65ff672f00728a
Hemant Bamboriya <dhakadhemant370@gmail.com>
2026-09-18 00:33:51 +0530

    Time: 3 ms (81.04%) | Memory: 11.5 MB (85.26%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.cpp
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.cpp
@@ -2,28 +2,29 @@ class Solution {
 public:
     vector<int> findAnagrams(string s, string p) {
         vector<int>result;
-        if(p.length()>s.length()){
+        if(p.size()>s.size()){
             return result;
         }
-        vector<int>freqP(26,0);
-        vector<int>windFreq(26,0);
-        for(char ch:p){
-            freqP[ch-'a']++;
+        vector<int>freq1(26,0);
+        vector<int>freq2(26,0);
+        for(int i=0;i<p.size();i++){
+            freq1[p[i]-'a']++;
         }
-        int k=p.length();
+        int k=p.size();
         for(int i=0;i<k;i++){
-            windFreq[s[i]-'a']++;
+            freq2[s[i]-'a']++;
         }
-        if(windFreq==freqP){
+        if(freq1==freq2){
             result.push_back(0);
         }
-        for(int i=k;i<s.length();i++){
-            windFreq[s[i]-'a']++;
-            windFreq[s[i-k]-'a']--;
-            if(windFreq==freqP){
+        for(int i=k;i<s.size();i++){
+            freq2[s[i]-'a']++;
+            freq2[s[i-k]-'a']--;
+            if(freq1==freq2){
                 result.push_back(i-k+1);
             }
         }
         return result;
+        
     }
 };
